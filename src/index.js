94d15e80c7278f41346818212731d8b532451281
Marcus <marcus.itec@gmail.com>
2019-03-07 20:49:56 -0300

    new dependencies in project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
-const soma = (a, b) => a + b
+import React, { Component, Fragment } from "react";
+import { render } from "react-dom";
 
-alert(soma(2, 8))
+import PropTypes from "prop-types";
+
+class Button extends Component {
+  static defaultProps = {
+    children: "Salvar"
+  };
+  
+  static propTypes = {
+    onClick: PropTypes.func.isRequired,
+    children: PropTypes.string
+  };
+
+  render() {
+    return (
+      <a href="" onClick={this.props.onClick}>
+        {this.props.children}
+      </a>
+    );
+  }
+}
+
+class App extends Component {
+  handleClick() {
+    alert("Botão clicado!!!");
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <h1>Ola mundo!!!</h1>
+        <Button
+          onClick={() => {
+            alert("Botão salvar!");
+          }}
+        />
+        <Button onClick={ this.handleClick }>Enviar</Button>
+      </Fragment>
+    );
+  }
+}
+
+render(<App />, document.getElementById("app"));
